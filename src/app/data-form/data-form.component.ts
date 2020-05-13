44f3a408a3a9d2289b98afcbb562cee81dc42e4c
Ana Luisa <najera.analuisa>
2020-05-13 08:22:51 -0300

    Validação CSS

--- src/app/data-form/data-form.component.ts
+++ src/app/data-form/data-form.component.ts
@@ -1,9 +1,11 @@
 
 
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { map } from "rxjs/operators";
+import { format } from 'url';
+import { prepareEventListenerParameters } from '@angular/compiler/src/render3/view/template';
 
 
 @Component({
@@ -27,11 +29,11 @@ export class DataFormComponent implements OnInit {
   //});
   
     this.formulario = this.formBuilder.group({
-      nome: [null],
-      email: [null]
+      nome: [null, Validators.required],
+      email: [null, [Validators.required, Validators.email]]
     });
-
-    
+    //Mais de uma validação para o mesmo campo
+    // nome: [null, [Validators.required, Validators.minLength(3), Validators.maxLength(255)]]
   }
 
   onSubmit(){
@@ -40,7 +42,26 @@ export class DataFormComponent implements OnInit {
     this.httpClient.post('https://httpbin.org/post', 
                     JSON.stringify(this.formulario.value))
                     .pipe(map(res => res))
-                    .subscribe(dados => console.log(dados));
+                    .subscribe(dados => {
+                      console.log(dados)
+                      //reset form
+                      //  this.resetar();
+                    }, (error: any) => alert ('erro'));
+                    
   }
 
+  resetar(){
+    event.preventDefault();
+    this.formulario.reset();
+  }
+
+  verificaValidTouched(campo: string){
+    return !this.formulario.get(campo).valid && this.formulario.get(campo).touched;
+  }
+
+  aplicaCssErro(campo: string){
+    return{
+      'invalid-feedback': this.verificaValidTouched(campo)
+    }
+  }
 }
